3c836fa38abbe465f38ae5566dc3bfef28973d6d
Eric Rochester <erochest@virginia.edu>
2013-02-08 23:13:55 -0500

    Read in github auth from environment variables.

--- GitReview.hs
+++ GitReview.hs
@@ -6,12 +6,15 @@ module Main where
 
 import           Control.Applicative
 import           Control.Error
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.Configurator as C
 import           Data.Configurator.Types
 import           Data.Data
+import           Github.Api
 import           Github.Data
 import           Github.Review
 import           System.Console.CmdArgs
+import           System.Environment
 import           Text.Parsec (parse)
 import           Text.ParserCombinators.Parsec.Rfc2822 (NameAddr(..), address_list)
 
@@ -66,15 +69,25 @@ ghError = fmapL (UserError . show)
 ghErrorT :: Show a => EitherT a IO b -> GithubInteraction b
 ghErrorT = fmapLT (UserError . show)
 
+ghIO :: IO a -> GithubInteraction a
+ghIO = ghErrorT . tryIO
+
+newMsg :: String -> GithubInteraction a -> GithubInteraction a
+newMsg msg = fmapLT (const (UserError msg))
+
 -- Main
 
 main :: IO ()
 main = do
     result <- runGithubInteraction $ do
-        cfg <-  getReviewConfig
-            =<< (ghErrorT . tryIO $   C.load
-                                  =<< (:[]) . C.Required . config
-                                  <$> cmdArgs gitReviewCliArgs)
+        auth <- newMsg "Missing authentication environment variables \
+                       \(GITHUB_USER and GITHUB_PASSWD)."
+                (GithubBasicAuth <$> ghIO (BS.pack <$> getEnv "GITHUB_USER")
+                                 <*> ghIO (BS.pack <$> getEnv "GITHUB_PASSWD"))
+        cfg  <-  getReviewConfig
+             =<< ghIO (   C.load
+                      =<< (:[]) . C.Required . config
+                      <$> cmdArgs gitReviewCliArgs)
         liftIO $ print cfg
     print result
 
